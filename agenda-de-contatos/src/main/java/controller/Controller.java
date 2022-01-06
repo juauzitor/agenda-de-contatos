464ac7bee838103f72023e2e0aa892e99266eb64
Joao Vitor <juauzitor>
2022-01-06 15:45:48 -0300

    CRUD insert

--- agenda-de-contatos/src/main/java/controller/Controller.java
+++ agenda-de-contatos/src/main/java/controller/Controller.java
@@ -7,16 +7,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import model.DAO;
+import model.JavaBeans;
 
 /**
  * Servlet implementation class Controller
  */
 //@WebServlet(urlPatterns = {"/Controller", "/main"}) Foi o que o professor colocou entretanto não funcionou
 //@WebServlet(urlPatterns = {"/Controller"}) Aparentemente o problema a a requisição do Controller
-@WebServlet(urlPatterns = { "/main" }) // Apenas chamando a main vai.
+@WebServlet(urlPatterns = { "/main", "/insert" }) // Apenas chamando a main vai.
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -40,6 +42,10 @@ public class Controller extends HttpServlet {
 		System.out.println(action);
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 
@@ -47,5 +53,23 @@ public class Controller extends HttpServlet {
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
+	}
+
+	// Novo contatos
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		/** teste de recebimento do form
+		System.out.println(request.getParameter("nome"));
+		System.out.println(request.getParameter("fone"));
+		System.out.println(request.getParameter("email"));
+		**/
+		//setar as variavels do javabeans
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		// invocar o método de inserirContato passando o objeto contato
+		dao.inserirContato(contato);
+		// redirecionar para o documento agenda.jsp
+		response.sendRedirect("main");
 	}
 }
--- agenda-de-contatos/src/main/java/model/DAO.java
+++ agenda-de-contatos/src/main/java/model/DAO.java
@@ -2,16 +2,17 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DAO {
 	/** Módulo de conexão **/
-	//Parâmetros de conexão
+	// Parâmetros de conexão
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://localhost:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
 	private String password = "mysql2022";
-	
-	//Métodos de conexão
+
+	// Métodos de conexão
 	private Connection conectar() {
 		Connection con = null;
 		try {
@@ -23,15 +24,30 @@ public class DAO {
 			return null;
 		}
 	}
-	/** pode ficar comentado pois era só para teste
-	// teste de conexão
-	public void testeConexao() {
+
+	/**
+	 * pode ficar comentado pois era só para teste // teste de conexão public void
+	 * testeConexao() { try { Connection con = conectar(); System.out.println(con);
+	 * con.close(); } catch (Exception e) { System.out.println(e); } }
+	 **/
+	/** CRUD CREATE **/
+	public void inserirContato(JavaBeans contato) {
+		String create = "insert into contatos (nome,fone,email) values(?,?,?)";
 		try {
+			// abrir a conexão com o banco
 			Connection con = conectar();
-			System.out.println(con);
+			// Preparar a query para execução no banco de dados
+			PreparedStatement pst = con.prepareStatement(create);
+			// Subistituir os parâmetros (?) pelo conteúdo das variáveis
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+			// Executar a query
+			pst.executeUpdate();
+			// Encerrar a concexão com o banco
 			con.close();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-	}**/
+	}
 }
